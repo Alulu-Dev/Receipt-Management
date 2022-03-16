8f4d1ea746fa0a50c54c7f4b56cb75c76cf8832b
Eshtaol Girma Bayessa <it.eshtaol.girma@gmail.com>
2022-03-16 10:48:44 +0300

    model blueprint

--- RM Back-End/API/__init__.py
+++ RM Back-End/API/__init__.py
@@ -4,10 +4,7 @@ from .settings import MONGO_URI
 from .apis import api as api_namespace
 
 app = Flask(__name__)
-# app.config['MONGODB_SETTINGS'] = {
-#     'host': MONGO_URI,
-#     'db': 'ReceiptManagement'
-# }
+
 app.config['MONGODB_SETTINGS'] = {
     'db': 'ReceiptManagement',
     'host': '127.0.0.1',
--- RM Back-End/API/core/DatabaseModel.py
+++ RM Back-End/API/core/DatabaseModel.py
@@ -5,18 +5,6 @@ from mongoengine import *
 from datetime import datetime
 
 
-class UserType(Document):
-    user_type = IntField(min_value=0, max_value=2)
-    type_description = StringField(required=True)
-#     0 for customer, 1 for admin, 2 for superAdmin
-
-
-class UserStatus(Document):
-    user_status = IntField(min_value=0, max_value=2)
-    status_description = StringField(required=True)
-#     0 for active, 1 for blocked, 2 for deleted
-
-
 class User(Document):
     username = StringField(required=True, unique=True,  min_length=3)
     first_name = StringField(required=True, max_length=50)
@@ -25,8 +13,6 @@ class User(Document):
     password = StringField(required=True)
     profile_picture = ImageField(collection_name='Avatars', null=True)
     data_created = DateTimeField(default=datetime.utcnow)
-    # status = ReferenceField('UserStatus', required=True)
-    # type = ReferenceField('UserType', required=True)
     status = StringField(required=True, default='Customer', enumerate=['Customer', 'Admin', 'SuperAdmin'])
     type = StringField(required=True, default='Active', enumerate=['Active', 'Blocked', 'Deleted'])
 
@@ -36,10 +22,6 @@ class User(Document):
     }
 
 
-# class ProfileAvatar:
-#     pass
-#
-#
 class Receipt(Document):
     """
     TIN Number
@@ -66,7 +48,7 @@ class PurchasedItems(Document):
 
 class ItemsDictionary(Document):
     """
-    Standard Name
+    Standard Name / Tag
     default == uncategorized
     """
     pass
@@ -86,8 +68,6 @@ class ExpenseSummary:
     Description
     """
     pass
-#
-#
 
 
 class FraudReport:
@@ -101,24 +81,34 @@ class FraudReport:
 
     """
     pass
-#
-#
-# class PredictionReport:
-#     pass
-#
-#
-# class PriceComparison:
-#     pass
-#
-#
-# class UserRequest:
-#     pass
-#
-#
-# class ReceiptItems:
-#     pass
-#
-#
+
+
+class PredictionReport:
+    """
+    User ID
+    Item Tag
+    ProbabilityPercent
+    Data Range
+    """
+    pass
+
+
+class PriceComparison:
+    """
+    Item Tag
+    Best Price
+    Business PLace
+    """
+    pass
+
+
+class UserRequest:
+    """
+    User ID
+    Request Resolved
+    Receipt ID
+    """
+    pass
 
 
 class ERCARecord:
