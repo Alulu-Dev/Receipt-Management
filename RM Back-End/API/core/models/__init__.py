04d138b07f15b0744f9f1daf4ec93fb11755859f
mikiyas daniel <it.mikiyas.daniel@gmail.com>
2022-03-31 22:50:20 +0300

    test input temepletes

--- RM Back-End/API/core/models/__init__.py
+++ RM Back-End/API/core/models/__init__.py
@@ -1,3 +1,4 @@
+# from .inputTemplates import new_user_fields as user_fields
 from .databaseModels import (User as accountModel, AccountStatusLog as StatusLog,
                              Receipt as receiptDataModel,
                              Items as receiptItems,
@@ -7,3 +8,5 @@ from .inputTemplates import (signup_input_parser as signup_form,
                              update_input_parser as update_form,
                              account_id_parser as id_form,
                              status as status_form)
+from .testInputTemplate import (receipt_parser as receipt_input_form,
+                                receipt_form, receipt_model, items_model)
